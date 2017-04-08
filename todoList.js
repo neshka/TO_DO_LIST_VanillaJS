c7c9d3b870372a4f031d3d446e665acdba938e18
neshka <neshka_mind@yahoo.com>
2017-04-08 22:30:10 +0100

    add comments

--- todoList.js
+++ todoList.js
@@ -1,17 +1,26 @@
 var todos = ['item 1', 'item 2', 'item 3'] //creating variables
 
+//it should have a function to display todos
 function displayTodos() { 
 	console.log(todos);
 }
 
+
+//it should have a function to add a new todo
 function addTodo(todo){
 	todos.push(todo);
 	displayTodos();
 }
 
-//change todo function
+//it should have a function to change todo
 function changeTodo (position, newValue) {
 	todos[position] = newValue;
+	displayTodos();
+}
+
+//it should have a function to delete todo
+function deleteTodo (startPosition){
+	todos.splice(startPosition,1);
 	displayTodos();
 }
 
