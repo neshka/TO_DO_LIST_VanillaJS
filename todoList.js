d0ba90a9cee9d94f9ec5a679922e6532b32906a2
neshka <neshka_mind@yahoo.com>
2017-07-06 23:46:23 +0100

    add new function

--- todoList.js
+++ todoList.js
@@ -105,7 +105,13 @@ var handlers = {
 };
 
 
-
+var view = {
+  displayTodos: function() {
+    var todosUl = document.querySelector('ul');
+    var todoLi = document.createElement('li');
+    todosUl.appendChild(todoLi);
+  }
+};
 
 
 
