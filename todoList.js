60c574922c18f16084040a0d66570bd757f70729
neshka <neshka_mind@yahoo.com>
2017-07-11 23:26:12 +0100

    add escape from the console

--- todoList.js
+++ todoList.js
@@ -1,48 +1,28 @@
 var todoList = {
   
   todos: [],
-  //it should have a method to display todos
-  displayTodos: function() {       
-    if (this.todos.length===0) {
-      console.log(' My todos list is empty.')
-    }else{
-      console.log('My todos:');
-      for (var i = 0; i < this.todos.length; i++) {
-        if (this.todos[i].completed === false) {
-          console.log('( )', this.todos[i].todoText);
-        } else {
-          console.log('(x)', this.todos[i].todoText);
-        }
-      }
-    }
-  },
-
   //it should have a method to add a new todo
   addTodo: function(todoText){
     this.todos.push({
       todoText: todoText,
       completed: false
     });
-    this.displayTodos();
   },
   
   //it should have a function to change todo
   changeTodo: function(position,todoText){
     this.todos[position].todoText = todoText;
-    this.displayTodos();
   },
 
   //it should change completed property
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
-    this.displayTodos();
   },
   
   //it should have a function to delete todo
   deleteTodo: function(position){
     this.todos.splice(position,1);
-    this.displayTodos();
   },
 
   toggleAll: function() {
@@ -66,20 +46,16 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-
-    this.displayTodos();
   }
   
 };
 
 var handlers = {
-  displayTodos: function () {
-    todoList.displayTodos();
-  },
   addTodo: function () {
     var addTodoTextInput = document.getElementById('addTodoTextInput');
     todoList.addTodo(addTodoTextInput.value);
     addTodoTextInput.value = "";//after we add value the input will stay empty
+    view.displayTodos();
   },
   changeTodo: function(){
     var changeTodoPositionInput = document.getElementById('changeTodoPositionInput');
@@ -87,20 +63,24 @@ var handlers = {
     todoList.changeTodo(changeTodoPositionInput.valueAsNumber, changeTodoTextInput.value);
     //clear values
     changeTodoPositionInput.value = "";
-    changeTodoTextInput.value = "";    
+    changeTodoTextInput.value = ""; 
+    view.displayTodos();   
   },
   toggleCompleted: function(){
     var toggleCompleted = document.getElementById('toggleCompleted');
     todoList.toggleCompleted(toggleCompleted.valueAsNumber);
     toggleCompleted.value = "";
+    view.displayTodos();
   },
   deleteTodo: function(){
     var deleteTodo = document.getElementById('deleteTodo');
     todoList.deleteTodo(deleteTodo.valueAsNumber);
     deleteTodo.value = "";
+    view.displayTodos();
   },
   toggleAll: function () {
     todoList.toggleAll();
+    view.displayTodos();
   },
 };
 
