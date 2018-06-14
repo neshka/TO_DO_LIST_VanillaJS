871482f6658d47c93ae71d8a763cc368781f4fff
neshka <neshka_mind@yahoo.com>
2018-06-14 20:27:25 +0100

    add changes

--- todoList.js
+++ todoList.js
@@ -86,23 +86,37 @@ var view = {
   displayTodos: function() {
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for (var i = 0; i < todoList.todos.length; i++) {      
-      var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
+    // for (var i = 0; i < todoList.todos.length; i++) {      
+    //   var todoLi = document.createElement('li');
+    //   var todo = todoList.todos[i];
 
+    //   var todoTextWithCompletion = '';
+    //   if (todo.completed === true) {
+    //     todoTextWithCompletion = '(x) ' + todo.todoText;
+    //   } else {
+    //     todoTextWithCompletion = '( ) ' + todo.todoText;
+    //   }
+
+    //   todoLi.id = i;
+    //   todoLi.textContent = todoTextWithCompletion;
+    //   todoLi.appendChild(this.createDeleteButton());
+    //   todosUl.appendChild(todoLi);
+    // }
+    todosList.todos.forEach(function(todo, position) {
+      var todoLi = document.createElement('li');
       var todoTextWithCompletion = '';
+
       if (todo.completed === true) {
         todoTextWithCompletion = '(x) ' + todo.todoText;
       } else {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
-
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    });
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement('button');
